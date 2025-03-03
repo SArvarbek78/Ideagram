063b7311cf19501a0b31cf7e83af8fae5bbc07d1
SArvarbek78 <urazovsarvar95@gmail.com>
2025-03-03 17:14:26 +0300

    Sayt yuklandi

---
+++ script.js
@@ -0,0 +1,3 @@
+function salom() {
+    alert("Ushbu Web Site Cristiano Ronaldo uchun atalgan!Va bu mening birinchi saytim.Bu saytda faqatgina Cristiano Ronaldo haqida yozilgan ammo kelajakda bu sayt juda rivojlanadi!");
+}
